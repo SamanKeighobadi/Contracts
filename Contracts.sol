1debcb333c55c2b7bab1f838264cf5ea1b7e40ac
saman keighobadi <saman.keighobadi@gmail.com>
2022-02-18 18:57:49 +0330

    definition and init variables

--- Contracts.sol
+++ Contracts.sol
@@ -2,6 +2,22 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Contracts{
-    
+contract Contracts {
+    uint256 public startProjectDate;
+    uint16 public day;
+    uint256 public amount = 1000;
+    uint256 public deposit = 100;
+    address payable employer;
+    address payable contractor;
+    address trusted;
+
+    enum Status {
+        noStarted,
+        Started,
+        Ended,
+        Failed,
+        Paid,
+        Suspended
+    }
+    Status currentStatus = Status.noStarted;
 }
